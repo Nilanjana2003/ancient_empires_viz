5fee8c79eb10e5fed2f2079f36501415f02c7f98
Nilanjana <nilanjana20082003@gmail.com>
2025-12-14 13:43:24 +0530

    added the backend and some year features to get the data using the year

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import Flask,jsonify,request
+import pandas as pd
+app = Flask(__name__)
+@app.route("/empires")
+def get_empires():
+    df = pd.read_csv("C:/Users/Nila/Desktop/pythonprojects/ancient_empires_viz/data/clean_empire_data.csv")
+    year = request.args.get("year",type = int)
+    if year is not None:
+        df = df[
+            (df["start_year_bce"]>= year) &
+            (df["end_year_bce"]<=year)
+            
+        ]
+    return jsonify(df.to_dict(orient = "records"))
+if __name__ == "__main__":
+    app.run(debug = True)
+    
