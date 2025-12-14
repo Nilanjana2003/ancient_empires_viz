eef3e75d0fa9a2a40e3b476736434eae6a3cf2ea
Nilanjana <nilanjana20082003@gmail.com>
2025-12-14 18:48:29 +0530

    this is the app py backend after resolving the CORS issue

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import Flask,jsonify,request
+from flask_cors import CORS
 import pandas as pd
 app = Flask(__name__)
+CORS(app)
 @app.route("/empires")
 def get_empires():
     df = pd.read_csv("C:/Users/Nila/Desktop/pythonprojects/ancient_empires_viz/data/clean_empire_data.csv")
